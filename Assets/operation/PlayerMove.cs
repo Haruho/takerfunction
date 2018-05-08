2e865faa9295901e23ad4bb3b7eb5a9b25a82751
Haruho <532685083@qq.com>
2018-05-09 00:09:57 +0800

    player movement function by rigibody

---
+++ Assets/operation/PlayerMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+    public float move;
+    private Rigidbody2D rg;
+
+	// Use this for initialization
+	void Start () {
+        rg = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rg.velocity = new Vector2(-move,0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rg.velocity = new Vector2(move, 0);
+        }
+    }
+}
